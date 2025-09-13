df6283e0fbe3fb64b369fc2eb7ee5e7a3ce9c2b9
Carolyn <117613176+carolyn-sun@users.noreply.github.com>
2025-09-14 00:49:15 +0800

    update setup script to configure git lfs, user info, and gpg settings

--- setup.sh
+++ setup.sh
@@ -4,29 +4,43 @@
 # shellcheck disable=SC2155
 export PATH="$PATH:$(pwd)"
 
-# set proxy for zsh
+# shell proxy
+## set proxy for zsh
 echo 'export all_proxy="http://127.0.0.1:6152"' >> ~/.zprofile
-
-# set proxy for bash
-# shellcheck disable=SC2016
+## set proxy for bash
+## shellcheck disable=SC2016
 echo 'export http_proxy="127.0.0.1:6152";export https_proxy=$http_proxy' >> ~/.bashrc
 
-# install brew
+# brew
+## install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# install app from brew bundle
+## install apps from brew bundle
 brew bundle --file=./Brewfile
 
-# set proxy for git
+# git
+## set proxy for git
 git config --global http.proxy http://127.0.0.1:6152
 git config --global https.proxy http://127.0.0.1:6152
+## git lfs filter
+git config --global filter.lfs.clean "git-lfs clean -- %f"
+git config --global filter.lfs.smudge "git-lfs smudge -- %f"
+git config --global filter.lfs.process "git-lfs filter-process"
+git config --global filter.lfs.required true
+## git user info
+git config --global user.email "117613176+carolyn-sun@users.noreply.github.com"
+git config --global user.name "Carolyn"
+git config --global user.signingkey CF65594A9A28BFF655A6D862DED2EA3A5A20EEB9
+## signature
+git config --global commit.gpgsign true
+## gpg app path
+git config --global gpg.program "/usr/local/MacGPG2/bin/gpg"
 
-# set default text editor
+# default apps
+## set default text editor
 duti -s com.microsoft.VSCode public.plain-text all
 duti -s com.microsoft.VSCode public.data all
 duti -s com.microsoft.VSCode md all
-
-# set default archive utility
+## set default archive utility
 duti -s com.aone.keka public.zip-archive all
 duti -s com.aone.keka com.rarlab.rar-archive all
 duti -s com.aone.keka org.7-zip.7-zip-archive all
