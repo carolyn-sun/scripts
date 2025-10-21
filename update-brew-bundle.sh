68b890b520a984c4b8fefe51117de9cb5702be62
Carolyn Sun <117613176+carolyn-sun@users.noreply.github.com>
2025-10-21 12:57:25 +0800

    update brewfile

--- update-brew-bundle.sh
+++ update-brew-bundle.sh
@@ -1,3 +1,11 @@
 #!/bin/zsh
+brew update
+brew upgrade
+brew cleanup
 rm -f ./Brewfile
 brew bundle dump --file=./Brewfile --describe
+if [[ $(git status --porcelain) ]]; then
+    git add ./Brewfile
+    git commit -m "Update Brewfile"
+    git push origin main
+fi
